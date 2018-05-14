7eb0e4d533c6df1246f557f187825c513d11b635
Nayra Lopes <nayra.teixeira@redes.unb.br>
2018-05-14 18:00:20 -0300

    [cnpq] Tratando o retorno para retornar o conteudo formatado para json.

--- app/Http/Controllers/Captura/LicitacaoCnpqController.php
+++ app/Http/Controllers/Captura/LicitacaoCnpqController.php
@@ -18,8 +18,67 @@ class LicitacaoCnpqController extends Controller
 
         $htmlCaptura = file_get_contents($url);
 
-        return $htmlCaptura;
+        $arrRetornoTratado = $this->tratandoHtmlApresentacao($htmlCaptura);
 
+        
+        return $arrRetornoTratado;
+
+    }
+    
+    /**
+     * 
+     * @param type $html
+     * @return array
+     */
+    private function tratandoHtmlApresentacao($html)
+    {
+
+        $doc = new \DOMDocument();
+
+        @$doc->loadHTML($html);
+
+        $tables = $doc->getElementsByTagName('tbody');
+
+        $rows = $tables->item(0)->getElementsByTagName('tr');
+
+        $arrLicitacoes = [];
+
+        // loop over the table rows
+        foreach ($rows as $row)
+        {
+            // get each column by tag name
+            $cols = $row->getElementsByTagName('td');
+
+            //Interando o arry de retornn
+            array_push($arrLicitacoes, $this->tratandoRetorno($cols));
+        }
+
+        return $arrLicitacoes;
+    }
+    
+        /**
+     * 
+     * @param type $cols
+     * @return array
+     */
+    private function tratandoRetorno($cols){        
+        $objeto = explode('Objeto', $cols->item(0)->nodeValue);
+        $abertura = explode('Abertura', $objeto[1]);
+        $publicacoes = explode('Publicações', $abertura['1']);
+
+//            dd(strip_tags($objeto[0]), $abertura[0], $publicacoes[0]);
+
+        $objeto = $objeto[0];
+        $abertura = $abertura[0];
+        $publicacoes = $publicacoes[0];
+
+        $arrLicitacao = [
+            'objeto'=>$objeto,
+            'abertura'=>$abertura,
+            'publicacoes'=>$publicacoes,
+        ];
+
+        return $arrLicitacao;
     }
 }
 
