b848c6bccccb33e9988166e4924f6a226d7fe542
Nayra Lopes <nayra.teixeira@redes.unb.br>
2018-05-14 17:52:31 -0300

    [cnpq] Criando a controller LicitacaoCnpqController

---
+++ app/Http/Controllers/Captura/LicitacaoCnpqController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Captura;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LicitacoesCnpqController extends Controller
+{
+    
+}
+
